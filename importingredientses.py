a228d887e6f0bb041893fff2ed5adaf8a69392ff
benwilder <benwilder@BenWildnMacBook.tagman.com>
2016-11-28 10:34:16 +0000

    Import for ingredients

---
+++ importingredientses.py
@@ -0,0 +1,32 @@
+import pymysql
+import uuid
+import sys
+import os
+import tempfile
+from ingredient_phrase_tagger.training import utils
+import subprocess
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+
+es = Elasticsearch()
+conn = pymysql.connect(host='localhost', port=3306, user='dev_recipe', passwd='devrecipe', db='dev_recipe')
+cur = conn.cursor(pymysql.cursors.DictCursor)
+
+print 'Importing into elastic search'
+
+# Select all ingredients for import
+selectIngredients = """
+SELECT 
+name,displayname
+FROM 
+recipeingredient
+"""
+
+cur.execute(selectIngredients)
+for row in cur:
+	doc = {
+    'ingredient': row['name'],
+    'ingredientdisplayname': row['displayname']
+	}
+	res = es.index(index='recipedb', doc_type='ingredient', body=doc)
