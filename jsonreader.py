10bad87bf7e8d11ceb74bc564c904d888a4d6433
benwilder <benwilder@Ben-Wilder-Ensighten-MacBook.local>
2016-12-03 12:34:34 +0000

    Another import and update reader to process files and unicode

--- jsonreader.py
+++ jsonreader.py
@@ -1,11 +1,19 @@
 import json
+import sys
+import os
 from pprint import pprint
 
-with open('bbcgoodfood_1000_export_results.json') as data_file:    
+if len(sys.argv) < 2:
+    sys.stderr.write('Usage: jsonreader.py FILENAME')
+    sys.exit(1)
+
+FILENAME = str(sys.argv[1])
+
+with open(FILENAME) as data_file:    
     data = json.load(data_file)
 
 for i in data['results']:
     if 'name' in i:
-        print(i['name'])
+        print(i['name'].encode('utf-8'))
     else:
     	print("blank")
