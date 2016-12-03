c4abfebfb2f680da654a237fa604beee2f240f8e
benwilder <benwilder@Ben-Wilder-Ensighten-MacBook.local>
2016-12-03 13:40:10 +0000

    Support for unicode import

--- importes.py
+++ importes.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+from __future__ import unicode_literals
 import pymysql
 import uuid
 import sys
@@ -9,8 +11,9 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 
 
+
 es = Elasticsearch()
-conn = pymysql.connect(host='localhost', port=3306, user='dev_recipe', passwd='devrecipe', db='dev_recipe')
+conn = pymysql.connect(host='localhost', port=3306, user='dev_recipe', passwd='devrecipe', db='dev_recipe',charset='utf8')
 cur = conn.cursor(pymysql.cursors.DictCursor)
 
 print 'Importing into elastic search'
@@ -18,23 +21,29 @@ print 'Importing into elastic search'
 # Select all recipes for import
 selectRecipes = """
 SELECT 
-scrapedrecipe.`name`,
-scrapedrecipe.`scrapedurl`,
-`recipeingredientsearchrecord`.`ingredientsrequired`,
-recipeingredientsearchrecord.`ingredientsrequiredcount`
+scrapedrecipe_current.`name`,
+scrapedrecipe_current.`scrapedurl`,
+`recipeingredientsearchrecord_current`.`ingredientsrequired`,
+recipeingredientsearchrecord_current.`ingredientsrequiredcount`
 FROM 
-scrapedrecipe INNER JOIN recipeingredientsearchrecord ON scrapedrecipe.`scrapedrecipeguid` = `recipeingredientsearchrecord`.`scrapedrecipeguid`
-ORDER BY scrapedrecipe.name ASC
+scrapedrecipe_current INNER JOIN recipeingredientsearchrecord_current ON scrapedrecipe_current.`scrapedrecipeguid` = `recipeingredientsearchrecord_current`.`scrapedrecipeguid`
+ORDER BY scrapedrecipe_current.name ASC
 """
 
 cur.execute(selectRecipes)
+
+i = 1
 for row in cur:
+	#ingredientList = row['ingredientsrequired'].encode('utf-8')
+	#print ingredientList
+	#print i, row['ingredientsrequired'].deccode('utf-8')
 	doc = {
     'title': row['scrapedurl'],
     'url': row['scrapedurl'],
-    'ingredientsrequired': row['ingredientsrequired'],
+    'ingredientsrequired': row['ingredientsrequired'].encode('utf-8'),
     'ingredientsrequiredcount': str(row['ingredientsrequiredcount'])
 	}
+	i=i+1
 	res = es.index(index='recipedb', doc_type='recipe', body=doc)
     #print(row['name'])
 #print(res['created'])
