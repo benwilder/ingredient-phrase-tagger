93dec505f23bd28ea97316a8a6a714a96609a872
benwilder <benwilder@Ben-Wilder-Ensighten-MacBook.local>
2016-12-03 13:42:09 +0000

    Unicode import support

--- importingredientses.py
+++ importingredientses.py
@@ -10,7 +10,7 @@ from elasticsearch import Elasticsearch
 
 
 es = Elasticsearch()
-conn = pymysql.connect(host='localhost', port=3306, user='dev_recipe', passwd='devrecipe', db='dev_recipe')
+conn = pymysql.connect(host='localhost', port=3306, user='dev_recipe', passwd='devrecipe', db='dev_recipe',charset='utf8')
 cur = conn.cursor(pymysql.cursors.DictCursor)
 
 print 'Importing into elastic search'
@@ -26,8 +26,8 @@ recipeingredient
 cur.execute(selectIngredients)
 for row in cur:
 	doc = {
-    'ingredient': row['name'],
-    'ingredientdisplayname': row['displayname'],
-    'ingredientautocompletename': row['autocompletename']
+    'ingredient': row['name'].encode('utf-8'),
+    'ingredientdisplayname': row['displayname'].encode('utf-8'),
+    'ingredientautocompletename': row['autocompletename'].encode('utf-8')
 	}
 	res = es.index(index='ingredientdb', doc_type='ingredient', body=doc)
