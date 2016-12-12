99b1fa1430a3d96894d7b791d2bae56d11d40835
benwilder <benwilder@benwildnmacbook.home>
2016-12-12 19:45:56 +0000

    Script to create recipes

---
+++ create_recipe_records.py
@@ -0,0 +1,20 @@
+import pymysql
+import uuid
+import sys
+import os
+import tempfile
+import re
+
+conn = pymysql.connect(host='localhost', port=3306, user='dev_recipe', passwd='devrecipe', db='dev_recipe', charset='utf8')
+cur = conn.cursor(pymysql.cursors.DictCursor)
+
+print "[1] Populating recipe table..."
+insertRecipes="""
+INSERT INTO ServeRecipe (RecipeName,IngredientNameModelledShortMatched,IngredientNameModelledShortMatchedCount)
+SELECT LearnRecipe.RecipeName, GROUP_CONCAT(LearnRecipeIngredient.`IngredientNameModelledShortMatched` SEPARATOR ' ') as IngredientsRequired, COUNT(LearnRecipeIngredient.`IngredientNameModelledShortMatched`) as IngredientsCount
+FROM LearnRecipeIngredient INNER JOIN LearnRecipe ON LearnRecipeIngredient.`RecipeGuid` = LearnRecipe.`Guid`
+GROUP BY LearnRecipeIngredient.`RecipeGuid`
+"""
+cur.execute(insertRecipes)
+conn.commit()
+print "[1] Table populated"
